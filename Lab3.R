270cadab78169c97fee39c9f6e9c72937fbf050a
Pragati Pant <pragatipant@heffner-wl-106.dynamic.rpi.edu>
2020-02-20 16:40:38 -0500

    First version of Lab3.

---
+++ Lab3.R
@@ -0,0 +1,52 @@
+library(rpart)
+library(rpart.plot)
+library(ggplot2)
+data(msleep)
+str(msleep)
+# observe the structure of the #msleep dataset
+str(data)
+
+
+####### Lab3_ctree1 ########
+require(rpart)
+Swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+plot(Swiss_rpart) # try some different plot options
+text(Swiss_rpart) # try some different text options
+
+library(rattle)   #using rattle for a different plotting option
+fancyRpartPlot(Swiss_rpart)
+
+require(party)
+
+treeSwiss<-ctree(Species ~ ., data=iris)
+plot(treeSwiss)
+
+cforest(Species ~ ., data=iris, controls=cforest_control(mtry=2, mincriterion=0))
+
+treeFert<-ctree(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+
+cforest(Fertility ~ Agriculture + Education + Catholic, data = swiss, controls=cforest_control(mtry=2, mincriterion=0))
+# look at help info, vary parameters.
+help("cforest")
+library(tree)
+tr <- tree(Species ~ ., data=iris)
+tr
+tr$frame
+plot(tr)
+text(tr)
+#find "prettier" ways to plot the tree
+
+####### Lab3_ctree2 ########
+# Conditional Inference Tree for Mileage
+fit2M <- ctree(Mileage~Price + Country + Reliability + Type, data=na.omit(cu.summary))
+summary(fit2M)
+# plot tree
+plot(fit2M, uniform=TRUE, main="CI Tree Tree for Mileage ")
+text(fit2M, use.n=TRUE, all=TRUE, cex=.8)
+
+####### Lab3_ctree3 ########
+fitK <- ctree(Kyphosis ~ Age + Number + Start, data=kyphosis)
+plot(fitK, main="Conditional Inference Tree for Kyphosis")
+plot(fitK, main="Conditional Inference Tree for Kyphosis",type="simple")
+
+

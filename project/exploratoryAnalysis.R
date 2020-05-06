3fa036779928a0597f70da8a8d41971f58103fa0
Pragati Pant <pragatipant@Pragatis-MBP.local>
2020-05-06 13:24:44 -0400

    Forgot to updated github with finished project files.

--- project/exploratoryAnalysis.R
+++ project/exploratoryAnalysis.R
@@ -21,56 +21,36 @@ Mean   :2006   Mean   :33.877
 3rd Qu.:2013   3rd Qu.:43.603                 
 Max.   :2018   Max.   :89.667  '
 
-hist(Value, main="Frequency of Education Levels in the World", xlab="Percentages of Educated Adults")   #initial reading: many countries have only 10%-50% of adults that are educated
-#need to break down by country, year, edu level
-
 USA_data <- adult_education_data[adult_education_data$LOCATION=="USA", ]
-USA_values_1981 <- USA_data[USA_data$TIME==1981, ]$Value
-hist(USA_values_1981)
-
-USA_values_2018 <- USA_data[USA_data$TIME==2018, ]$Value
-
-boxplot(USA_data$Value)
-boxplot(USA_values_1981, USA_values_2018, names=c("1981", "2018"), ylab="Years", xlab="Percentage of Adults", horizontal = TRUE)
-
-USA_data_TRY <- USA_data[USA_data$SUBJECT=="TRY", ]   #MADE THIS A LINE
-boxplot(USA_data_TRY$Value)
-USA_TRY_change <- plot(USA_data_TRY$Value~USA_data_TRY$TIME, main="Tertiary Education Level (USA)", xlab="Years", ylab="Percentage of Adults", type="l")
-        #there is a notable drop in 1992 for college educated ppl (double check what tertiary means)
+############ US data distributions ############
+USA_data_TRY <- USA_data[USA_data$SUBJECT=="TRY", ]
+USA_TRY_change <- plot(USA_data_TRY$Value~USA_data_TRY$TIME, main="Tertiary Education Level (USA)", xlab="Years", ylab="Percentage of Adults")
 
 USA_data_UPPSRY <- USA_data[USA_data$SUBJECT=="UPPSRY", ]
 USA_UPPSRY_change <- plot(USA_data_UPPSRY$Value~USA_data_UPPSRY$TIME, main="Upper Secondary Education Level (USA)", xlab="Years", ylab="Percentage of Adults")
-#there is a notable drop in 1992 for upper secondary educated ppl (double check what that means)
 
 USA_data_BUPPSRY <- USA_data[USA_data$SUBJECT=="BUPPSRY", ]
 USA_BUPPSRY_change <- plot(USA_data_BUPPSRY$Value~USA_data_BUPPSRY$TIME, main="Below Secondary Education Level (USA)", xlab="Years", ylab="Percentage of Adults")
-#there is a notable drop in 1992 for below secondary educated ppl (double check what that means)
 
 USA_data_TRY_MEN <- USA_data[USA_data$SUBJECT=="TRY_MEN", ]
 USA_TRY_change_MEN <- plot(USA_data_TRY_MEN$Value~USA_data_TRY_MEN$TIME, main="Tertiary Education Level for Men (USA)", xlab="Years", ylab="Percentage of Men")
-#there is a notable drop in 1992 for college educated ppl (double check what tertiary means)
 
 USA_data_UPPSRY_MEN <- USA_data[USA_data$SUBJECT=="UPPSRY_MEN", ]
 USA_UPPSRY_change_MEN <- plot(USA_data_UPPSRY_MEN$Value~USA_data_UPPSRY_MEN$TIME, main="Upper Secondary Education Level for Men (USA)", xlab="Years", ylab="Percentage of Men")
-#there is a notable drop in 1992 for upper secondary educated ppl (double check what that means)
 
 USA_data_TRY_WOMEN <- USA_data[USA_data$SUBJECT=="TRY_WOMEN", ]
 USA_TRY_change_WOMEN <- plot(USA_data_TRY_WOMEN$Value~USA_data_TRY_WOMEN$TIME, main="Tertiary Education Level for Women (USA)", xlab="Years", ylab="Percentage of Women")
-#there is a notable drop in 1992 for college educated ppl (double check what tertiary means)
 
 USA_data_UPPSRY_WOMEN <- USA_data[USA_data$SUBJECT=="UPPSRY_WOMEN", ]
 USA_UPPSRY_change_WOMEN <- plot(USA_data_UPPSRY_WOMEN$Value~USA_data_UPPSRY_WOMEN$TIME, main="Upper Secondary Education Level for Women (USA)", xlab="Years", ylab="Percentage of Women")
-#there is a notable drop in 1992 for upper secondary educated ppl (double check what that means)
 
 USA_TRY_change_GENDER <- plot(USA_data_TRY_WOMEN$Value~USA_data_TRY_WOMEN$TIME, main="Gendered Change in Tertiary Education Level (USA)", xlab="Years", ylab="Percentage of Adults", type="l", col="red", xlim=c(1980, 2020))
-#there is a notable drop in 1992 for college educated ppl (double check what tertiary means)
 lines(USA_data_TRY_MEN$Value~USA_data_TRY_MEN$TIME, col="blue")
 legend("topleft",
        c("Men","Women"),
        fill=c("blue","red"))
 
 USA_UPPSRY_change_GENDER <- plot(USA_data_UPPSRY_WOMEN$Value~USA_data_UPPSRY_WOMEN$TIME, main="Gendered Change in Upper Secondary Education Level (USA)", xlab="Years", ylab="Percentage of Adults", type="l", col="red", xlim=c(1980, 2020))
-#there is a notable drop in 1992 for college educated ppl (double check what tertiary means)
 lines(USA_data_UPPSRY_MEN$Value~USA_data_UPPSRY_MEN$TIME, col="blue")
 legend("topright",
        c("Men","Women"),
@@ -83,34 +63,82 @@ legend("topright",
        c("Below Upper Secondary","Upper Secondary", "Tertiary"),
        fill=c("blue","red", "green"))
 
-world_data_1981_TRY <- filter(adult_education_data, (adult_education_data$TIME==1981 & adult_education_data$SUBJECT=="TRY"))
+############ world data distributions ############
+world_data_TRY <- adult_education_data[adult_education_data$SUBJECT=="TRY", ]
+world_TRY_change <- plot(world_data_TRY$Value~world_data_TRY$TIME, main="Tertiary Education Level (world)", xlab="Years", ylab="Percentage of Adults", xlim=c(1980, 2020))
 
-world_data_1981_TRY_plot <- barplot(world_data_1981_TRY$Value, names.arg=world_data_1981_TRY$LOCATION, main="World Tertiary Education Level Percentages in 1981", xlab="Countries", ylab="Percentage of Adults", ylim=c(0, 30))
+world_data_UPPSRY <- adult_education_data[adult_education_data$SUBJECT=="UPPSRY", ]
+world_UPPSRY_change <- plot(world_data_UPPSRY$Value~world_data_UPPSRY$TIME, main="Upper Secondary Education Level (world)", xlab="Years", ylab="Percentage of Adults")
 
+world_data_BUPPSRY <- adult_education_data[adult_education_data$SUBJECT=="BUPPSRY", ]
+world_BUPPSRY_change <- plot(world_data_BUPPSRY$Value~world_data_BUPPSRY$TIME, main="Below Upper Secondary Education Level (world)", xlab="Years", ylab="Percentage of Adults")
 
+world_data_TRY_MEN <- adult_education_data[adult_education_data$SUBJECT=="TRY_MEN", ]
+world_TRY_change_MEN <- plot(world_data_TRY_MEN$Value~world_data_TRY_MEN$TIME, main="Tertiary Education Level for Men (world)", xlab="Years", ylab="Percentage of Men")
 
-world_data_2018_TRY <- filter(adult_education_data, (adult_education_data$TIME==2018 & adult_education_data$SUBJECT=="TRY" & adult_education_data$LOCATION!="OAVG"))
+world_data_UPPSRY_MEN <- adult_education_data[adult_education_data$SUBJECT=="UPPSRY_MEN", ]
+world_UPPSRY_change_MEN <- plot(world_data_UPPSRY_MEN$Value~world_data_UPPSRY_MEN$TIME, main="Upper Secondary Education Level for Men (world)", xlab="Years", ylab="Percentage of Men")
 
-world_data_2018_TRY_plot <- barplot(world_data_2018_TRY$Value, names.arg=world_data_2018_TRY$LOCATION, main="World Tertiary Education Level Percentages in 2018", xlab="Countries", ylab="Percentage of Adults", ylim=c(0, 50))
+world_data_TRY_WOMEN <- adult_education_data[adult_education_data$SUBJECT=="TRY_WOMEN", ]
+world_TRY_change_WOMEN <- plot(world_data_TRY_WOMEN$Value~world_data_TRY_WOMEN$TIME, main="Tertiary Education Level for Women (world)", xlab="Years", ylab="Percentage of Women")
 
-plot(world_data_2018_TRY$Value, names.arg=world_data_2018_TRY$LOCATION)
+world_data_UPPSRY_WOMEN <- adult_education_data[adult_education_data$SUBJECT=="UPPSRY_WOMEN", ]
+world_UPPSRY_change_WOMEN <- plot(world_data_UPPSRY_WOMEN$Value~world_data_UPPSRY_WOMEN$TIME, main="Upper Secondary Education Level for Women (world)", xlab="Years", ylab="Percentage of Women")
 
+world_TRY_change_GENDER <- plot(world_data_TRY_WOMEN$Value~world_data_TRY_WOMEN$TIME, main="Gendered Change in Tertiary Education Level (world)", xlab="Years", ylab="Percentage of Adults", type="l", col="red", xlim=c(1980, 2020))
+lines(world_data_TRY_MEN$Value~world_data_TRY_MEN$TIME, col="blue")
+legend("topleft",
+       c("Men","Women"),
+       fill=c("blue","red"))
 
+world_UPPSRY_change_GENDER <- plot(world_data_UPPSRY_WOMEN$Value~world_data_UPPSRY_WOMEN$TIME, main="Gendered Change in Upper Secondary Education Level (world)", xlab="Years", ylab="Percentage of Adults", type="l", col="red", xlim=c(1980, 2020))
+lines(world_data_UPPSRY_MEN$Value~world_data_UPPSRY_MEN$TIME, col="blue")
+legend("topright",
+       c("Men","Women"),
+       fill=c("blue","red"))
 
+world_change <- plot(world_data_UPPSRY$Value~world_data_UPPSRY$TIME, main="Education Level Changes (world)", xlab="Years", ylab="Percentage of Adults", type="l", col="red", xlim=c(1980, 2020), ylim=c(0, 80))
+lines(world_data_BUPPSRY$Value~world_data_BUPPSRY$TIME, col="blue")
+lines(world_data_TRY$Value~world_data_TRY$TIME, col="green")
+legend("topright",
+       c("Below Upper Secondary","Upper Secondary", "Tertiary"),
+       fill=c("blue","red", "green"))
 
+########### distribution plots ###########
+#world data histogram
+hist(Value, main="Frequency of Education Levels in the World", xlab="Percentages of Educated Adults")   #initial reading: many countries have only 10%-50% of adults that are educated
 
-library(dplyr)
-USA_values_1981_TRY <- filter(USA_data, (USA_data$TIME==1981 & USA_data$SUBJECT=="TRY"))
-USA_values_1981_TRY
-USA_years <- adult_education_data[adult_education_data$LOCATION=="USA",]$TIME
+#US data histogram
+hist(USA_data$Value, main="Frequency of Education Levels in the United States", xlab="Percentages of Educated Adults", xlim=c(10, 65))
 
-USA_data <- data.frame(USA_values, USA_years)
-USA_data
+#other exploratory analysis
+USA_values_1981 <- USA_data[USA_data$TIME==1981, ]$Value
+hist(USA_values_1981)
 
-hist(USA_values)
-plot(USA_data$USA_values~USA_data$USA_years)
+USA_values_2018 <- USA_data[USA_data$TIME==2018, ]$Value
+hist(USA_values_2018)
 
+#world boxplots
+boxplot(adult_education_data$Value)
+boxplot(world_data_BUPPSRY$Value, main="World Below Secondary Education Distribution", xlab="Percentages of Adults", ylab="Frequency")
+boxplot(world_data_UPPSRY$Value, main="World Upper Secondary Education Distribution", xlab="Percentages of Adults", ylab="Frequency")
+boxplot(world_data_TRY$Value, main="World Tertiary Education Distribution", xlab="Percentages of Adults", ylab="Frequency")
 
+boxplot(world_data_UPPSRY_MEN$Value, main="World Upper Secondary Education Distribution (Men)", xlab="Percentages of Adults", ylab="Frequency")
+boxplot(world_data_UPPSRY_WOMEN$Value, main="World Upper Secondary Education Distribution (Women)", xlab="Percentages of Adults", ylab="Frequency")
+boxplot(world_data_TRY_MEN$Value, main="World Tertiary Education Distribution (Men)", xlab="Percentages of Adults", ylab="Frequency")
+boxplot(world_data_TRY_WOMEN$Value, main="World Tertiary Education Distribution (Women)", xlab="Percentages of Adults", ylab="Frequency")
+
+#US boxplots
+boxplot(USA_data$Value, xlab="Percentages of Adults", ylab="Frequency")
+boxplot(USA_data_BUPPSRY$Value, main="United States Below Secondary Education Distribution", xlab="Percentages of Adults", ylab="Frequency")
+boxplot(USA_data_UPPSRY$Value, main="United States Upper Secondary Education Distribution", xlab="Percentages of Adults", ylab="Frequency")
+boxplot(USA_data_TRY$Value, main="United States Tertiary Education Distribution", xlab="Percentages of Adults", ylab="Frequency")
+
+boxplot(USA_data_UPPSRY_MEN$Value, main="United States Upper Secondary Education Distribution (Men)", xlab="Percentages of Adults", ylab="Frequency")
+boxplot(USA_data_UPPSRY_WOMEN$Value, main="United States Upper Secondary Education Distribution (Women)", xlab="Percentages of Adults", ylab="Frequency")
+boxplot(USA_data_TRY_MEN$Value, main="United States Tertiary Education Distribution (Men)", xlab="Percentages of Adults", ylab="Frequency")
+boxplot(USA_data_TRY_WOMEN$Value, main="United States Tertiary Education Distribution (Women)", xlab="Percentages of Adults", ylab="Frequency")
 
 
 
--- project/regressionModel.R
+++ project/regressionModel.R
@@ -56,6 +56,19 @@ summary(USA_linear_model_TRY)  #Multiple R-squared:  0.9613,	Adjusted R-squared:
 USA_linear_model_BUPPSRY <- lm(Value~TIME, data=USA_data_BUPPSRY)  #all usa values/years in model, need to break down by edu level
 summary(USA_linear_model_BUPPSRY)  #Multiple R-squared:  0.9171,	Adjusted R-squared:  0.9148 
 
+#US GENDER models
+USA_linear_model_UPPSRY_MEN <- lm(Value~TIME, data=USA_data_UPPSRY_MEN)  #all usa values/years in model, need to break down by edu level
+summary(USA_linear_model_UPPSRY_MEN)  #Multiple R-squared:  0.8194,	Adjusted R-squared:  0.8144 
+
+USA_linear_model_UPPSRY_WOMEN <- lm(Value~TIME, data=USA_data_UPPSRY_WOMEN)  #all usa values/years in model, need to break down by edu level
+summary(USA_linear_model_UPPSRY_WOMEN)  #Multiple R-squared:  0.9505,	Adjusted R-squared:  0.9491 
+
+USA_linear_model_TRY_MEN <- lm(Value~TIME, data=USA_data_TRY_MEN)  #all usa values/years in model, need to break down by edu level
+summary(USA_linear_model_TRY_MEN)  #Multiple R-squared:  0.9361,	Adjusted R-squared:  0.9343 
+
+USA_linear_model_TRY_WOMEN <- lm(Value~TIME, data=USA_data_TRY_WOMEN)  #all usa values/years in model, need to break down by edu level
+summary(USA_linear_model_TRY_WOMEN)  #Multiple R-squared:  0.9718,	Adjusted R-squared:  0.971 
+
 #world models from 1981 to 2018
 world_model_TRY <- lm(Value~TIME, data=world_data_TRY)    #lm model with whole data 
 summary(world_model_TRY) #Multiple R-squared:  0.2062,	Adjusted R-squared:  0.2054    shows there is a weak linear relationship
@@ -93,6 +106,19 @@ summary(world_model_BUPPSRY) #Multiple R-squared:  0.1013,	Adjusted R-squared:  
 ############ model plots #################
 library(ggplot2)
 
+#world plots from 1981 to 2018
+world_BUPPSRY_regression_plot <- ggplot(world_model_BUPPSRY, aes(x=TIME, y=Value)) + geom_point(aes(color=world_data_BUPPSRY$LOCATION))+labs(color="Countries")+geom_smooth(method=lm)+ggtitle("World Below Upper Secondary Education Regression")+xlab("Years")+ylab("Percentage of Adults")
+world_BUPPSRY_regression_plot
+
+world_UPPSRY_regression_plot <- ggplot(world_model_UPPSRY, aes(x=TIME, y=Value)) + geom_point(aes(color=world_data_UPPSRY$LOCATION))+labs(color="Countries")+geom_smooth(method=lm)+ggtitle("World Upper Secondary Education Regression")+xlab("Years")+ylab("Percentage of Adults")
+world_UPPSRY_regression_plot
+
+world_UPPSRY_regression_plot_MEN <- ggplot(world_model_UPPSRY_MEN, aes(x=TIME, y=Value)) + geom_point(aes(color=world_data_UPPSRY$LOCATION))+labs(color="Countries")+geom_smooth(method=lm)+ggtitle("World Upper Secondary Education Regression (Men)")+xlab("Years")+ylab("Percentage of Adults")
+world_UPPSRY_regression_plot_MEN
+
+world_UPPSRY_regression_plot_WOMEN <- ggplot(world_model_UPPSRY_WOMEN, aes(x=TIME, y=Value)) + geom_point(aes(color=world_data_UPPSRY$LOCATION))+labs(color="Countries")+geom_smooth(method=lm)+ggtitle("World Upper Secondary Education Regression (Women)")+xlab("Years")+ylab("Percentage of Adults")
+world_UPPSRY_regression_plot_WOMEN
+
 world_TRY_regression_plot <- ggplot(world_model_TRY, aes(x=TIME, y=Value)) + geom_point(aes(color=world_data_TRY$LOCATION))+labs(color="Countries")+geom_smooth(method=lm)+ggtitle("World Tertiary Education Regression")+xlab("Years")+ylab("Percentage of Adults")
 world_TRY_regression_plot
 
@@ -102,7 +128,80 @@ world_TRY_regression_plot_MEN
 world_TRY_regression_plot_WOMEN <- ggplot(world_model_TRY_WOMEN, aes(x=TIME, y=Value)) + geom_point(aes(color=world_data_TRY$LOCATION))+labs(color="Countries")+geom_smooth(method=lm)+ggtitle("World Tertiary Education Regression (Women)")+xlab("Years")+ylab("Percentage of Adults")
 world_TRY_regression_plot_WOMEN
 
-ggplot(USA_linear_model_TRY, aes(x=TIME, y=Value)) + geom_point()+geom_smooth(method=lm)
+#US plots from 1981 to 2018
+ggplot(USA_linear_model_BUPPSRY, aes(x=TIME, y=Value)) + geom_point()+geom_smooth(method=lm)+ggtitle("United States Below Upper Secondary Education Regression")+xlab("Years")+ylab("Percentage of Adults")
 
 
+ggplot(USA_linear_model_UPPSRY, aes(x=TIME, y=Value)) + geom_point()+geom_smooth(method=lm)+ggtitle("United States Upper Secondary Education Regression")+xlab("Years")+ylab("Percentage of Adults")
+ggplot(USA_linear_model_UPPSRY_MEN, aes(x=TIME, y=Value)) + geom_point()+geom_smooth(method=lm)+ggtitle("United States Upper Secondary Education Regression (Men)")+xlab("Years")+ylab("Percentage of Adults")
+ggplot(USA_linear_model_UPPSRY_WOMEN, aes(x=TIME, y=Value)) + geom_point()+geom_smooth(method=lm)+ggtitle("United States Upper Secondary Education Regression (Women)")+xlab("Years")+ylab("Percentage of Adults")
 
+ggplot(USA_linear_model_TRY, aes(x=TIME, y=Value)) + geom_point()+geom_smooth(method=lm)+ggtitle("United States Tertiary Education Regression")+xlab("Years")+ylab("Percentage of Adults")
+ggplot(USA_linear_model_TRY_MEN, aes(x=TIME, y=Value)) + geom_point()+geom_smooth(method=lm)+ggtitle("United States Tertiary Education Regression (Men)")+xlab("Years")+ylab("Percentage of Adults")
+ggplot(USA_linear_model_TRY_WOMEN, aes(x=TIME, y=Value)) + geom_point()+geom_smooth(method=lm)+ggtitle("United States Tertiary Education Regression (Women)")+xlab("Years")+ylab("Percentage of Adults")
+
+#US predictions -- 2020
+test_2020_TRY <- predict(USA_linear_model_TRY, data.frame(TIME=2020))
+test_2020_TRY       #50.24887 of adults will belong to tertiary level
+
+test_2020_TRY_MEN <- predict(USA_linear_model_TRY_MEN, data.frame(TIME=2020))
+test_2020_TRY_MEN       #46.0794 of men will belong to tertiary level
+
+test_2020_TRY_WOMEN <- predict(USA_linear_model_TRY_WOMEN, data.frame(TIME=2020))
+test_2020_TRY_WOMEN       #54.24446 of women will belong to tertiary level
+
+test_2020_UPPSRY <- predict(USA_linear_model_UPPSRY, data.frame(TIME=2020))
+test_2020_UPPSRY       #42.3097 of adults will belong to tertiary level
+
+test_2020_UPPSRY_MEN <- predict(USA_linear_model_UPPSRY_MEN, data.frame(TIME=2020))
+test_2020_UPPSRY_MEN       #45.29563 of men will belong to tertiary level
+
+test_2020_UPPSRY_WOMEN <- predict(USA_linear_model_UPPSRY_WOMEN, data.frame(TIME=2020))
+test_2020_UPPSRY_WOMEN       #39.45767 of women will belong to tertiary level
+
+test_2020_BUPPSRY <- predict(USA_linear_model_BUPPSRY, data.frame(TIME=2020))
+test_2020_BUPPSRY       #7.441435 of adults will belong to below upper secondary level
+
+#US predictions -- 2050
+test_2050_TRY <- predict(USA_linear_model_TRY, data.frame(TIME=2050))
+test_2050_TRY       #73.85634 of adults will belong to tertiary level
+
+test_2050_TRY_MEN <- predict(USA_linear_model_TRY_MEN, data.frame(TIME=2050))
+test_2050_TRY_MEN       #62.98439 of men will belong to tertiary level
+
+test_2050_TRY_WOMEN <- predict(USA_linear_model_TRY_WOMEN, data.frame(TIME=2050))
+test_2050_TRY_WOMEN       #84.25239 of women will belong to tertiary level
+
+test_2050_UPPSRY <- predict(USA_linear_model_UPPSRY, data.frame(TIME=2050))
+test_2050_UPPSRY       #28.72742 of adults will belong to tertiary level
+
+test_2050_UPPSRY_MEN <- predict(USA_linear_model_UPPSRY_MEN, data.frame(TIME=2050))
+test_2050_UPPSRY_MEN       #37.56876 of men will belong to tertiary level
+
+test_2050_UPPSRY_WOMEN <- predict(USA_linear_model_UPPSRY_WOMEN, data.frame(TIME=2050))
+test_2050_UPPSRY_WOMEN       #20.29467 of women will belong to tertiary level
+
+test_2050_BUPPSRY <- predict(USA_linear_model_BUPPSRY, data.frame(TIME=2050))
+test_2050_BUPPSRY       #-2.583758 of adults will belong to below upper secondary level
+
+#US predictions -- 2075
+test_2075_TRY <- predict(USA_linear_model_TRY, data.frame(TIME=2075))
+test_2075_TRY       #93.52923 of adults will belong to tertiary level
+
+test_2075_TRY_MEN <- predict(USA_linear_model_TRY_MEN, data.frame(TIME=2075))
+test_2075_TRY_MEN       #77.07188 of men will belong to tertiary level
+
+test_2075_TRY_WOMEN <- predict(USA_linear_model_TRY_WOMEN, data.frame(TIME=2075))
+test_2075_TRY_WOMEN       #109.259 of women will belong to tertiary level
+
+test_2075_UPPSRY <- predict(USA_linear_model_UPPSRY, data.frame(TIME=2075))
+test_2075_UPPSRY       #17.40886 of adults will belong to tertiary level
+
+test_2075_UPPSRY_MEN <- predict(USA_linear_model_UPPSRY_MEN, data.frame(TIME=2075))
+test_2075_UPPSRY_MEN       #31.1297 of men will belong to tertiary level
+
+test_2075_UPPSRY_WOMEN <- predict(USA_linear_model_UPPSRY_WOMEN, data.frame(TIME=2075))
+test_2075_UPPSRY_WOMEN       #4.325503 of women will belong to tertiary level
+
+test_2075_BUPPSRY <- predict(USA_linear_model_BUPPSRY, data.frame(TIME=2075))
+test_2075_BUPPSRY       #-10.93809 of adults will belong to below upper secondary level
